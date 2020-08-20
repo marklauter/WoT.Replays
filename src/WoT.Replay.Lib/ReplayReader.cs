7b5a7bacf40af0a4400a5b7a75dba6e52e2ca8ed
Mark Lauter <Mark.Lauter@insight.com>
2020-08-20 13:01:43 -0400

    added todo

--- src/WoT.Replay.Lib/ReplayReader.cs
+++ src/WoT.Replay.Lib/ReplayReader.cs
@@ -17,16 +17,27 @@ namespace Wot.Replays
 
             using var file = new FileStream(fileName, FileMode.Open, FileAccess.Read);
             using var reader = new BinaryReader(file);
-            
+
             var magicNumber = reader.ReadInt32();
-            if(MagicNumber != magicNumber)
+            if (MagicNumber != magicNumber)
             {
                 throw new InvalidDataException($"file is not a wot replay. expected magic number: {MagicNumber}, actual: {magicNumber}");
             }
 
+            // todo: the block count might actually be bit encoded
+            // see https://github.com/raszpl/wotdecoder/blob/master/wotdecoder.py
+            // # filename= name of .wotreplay file
+            // # to_decode= bitmask of chunks you want decoded.
+            // # We do not just count blocks as they are in replay files. Instead we always decode
+            // # Bit 0 = first Json block, starting player list
+            // # Bit 1 = second Json block, simplified frag count
+            // # Bit 2 = pickle, proper battle result with damage numbers
+            // # 7(binary 111) means decode all three. 5(binary 101) means decode first Json and pikle.
+            // #
+            // # returns decoded_chunks[0:3], chunks bitmask, decoder status
             var blockCount = reader.ReadInt32();
             var blocks = new byte[blockCount][];
-            
+
             for (var i = 0; i < blockCount; ++i)
             {
                 var blockSize = reader.ReadInt32();
